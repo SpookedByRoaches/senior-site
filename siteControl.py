f67c26a1ece0f2c41adb6e873d11396a5834c2de
Hasan Alarbash <mr.awsome668@gmail.com>
2021-01-30 14:06:40 -0500

    second page

--- siteControl.py
+++ siteControl.py
@@ -2,4 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def helloWorld():
-    return "hello world"
+    return "dolla dolla bills yo"
+
+@app.route("/someOtherPage")
+def someOtherPage():
+    return "WOW TWO PAGES"
