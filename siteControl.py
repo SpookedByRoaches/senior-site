c6f2f1571f07c6286920bf4b5560a5152b1bffc3
Hasan Alarbash <mr.awsome668@gmail.com>
2021-01-30 15:30:24 -0500

    added stuff

--- siteControl.py
+++ siteControl.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import *
+from markupsafe import escape
+
 app = Flask(__name__)
 @app.route("/")
 def helloWorld():
-    return "dolla dolla bills yo"
+    return "dolla"
 
 @app.route("/someOtherPage")
 def someOtherPage():
     return "WOW TWO PAGES"
+
+@app.route("/user/<username>")
+def profile(username):
+    return "{}'s profile".format(escape(username))
+
+with app.test_request_context():
+    print(url_for("helloWorld"))
+    print(url_for("someOtherPage"))
+    print(url_for("profile", username="whatever"))
